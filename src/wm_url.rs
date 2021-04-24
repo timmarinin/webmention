6982f535b02ca307202ddaee0774e07e15771a44
Tim Marinin <mt@marinintim.com>
2021-04-24 21:43:40 +0300

    add tests for absolute urls

--- src/wm_url.rs
+++ src/wm_url.rs
@@ -1,15 +1,14 @@
 pub use url::Url;
 
 pub fn absolute_url(candidate_url: &str, original_url: &Url) -> Result<Url, url::ParseError> {
+    if "".eq(candidate_url) {
+        return Ok(original_url.clone());
+    }
+
     match Url::parse(candidate_url) {
         Ok(absolute_url) => Ok(absolute_url),
         Err(url::ParseError::RelativeUrlWithoutBase) => {
-            let mut url = original_url.clone();
-            let segments = url.path_segments().unwrap();
-            let last = segments.last().unwrap();
-            if !last.contains(".") {
-                url.path_segments_mut().unwrap().push("");
-            }
+            let url = original_url.clone();
             url.join(candidate_url)
         }
         Err(e) => Err(e),
@@ -33,6 +32,26 @@ mod test {
         assert_eq!(
             absolute_url(relative_path, &pretty_url).unwrap().as_str(),
             "https://marinintim.com/projects/api/webmention"
+        );
+    }
+
+    #[test]
+    fn test_absolute_url2() {
+        let base_url = Url::parse("https://webmention.rocks/test/22").unwrap();
+        let relative_path = "22/webmention";
+        assert_eq!(
+            absolute_url(relative_path, &base_url).unwrap().as_str(),
+            "https://webmention.rocks/test/22/webmention"
+        );
+    }
+
+    #[test]
+    fn test_absolute_url3() {
+        let base_url = Url::parse("https://webmention.rocks/test/23/page/wiKQ8pZzlN0q3hsIZADg").unwrap();
+        let relative_path = "webmention-endpoint/xXNLydslCJo3niJSfoXk";
+        assert_eq!(
+            absolute_url(relative_path, &base_url).unwrap().as_str(),
+            "https://webmention.rocks/test/23/page/webmention-endpoint/xXNLydslCJo3niJSfoXk"
         );
     }
 }
