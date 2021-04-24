28bf648d5394c5a72900c3bcfd78b2d8efa07854
Tim Marinin <mt@marinintim.com>
2021-04-24 22:33:02 +0300

    rm ignored test

--- src/sending.rs
+++ src/sending.rs
@@ -20,22 +20,3 @@ pub async fn fetch_links(u: &Url) -> Result<std::collections::HashSet<Url>, Webm
 
     Ok(links.into_iter().collect())
 }
-
-#[cfg(test)]
-mod test {
-    use crate::webmention::Webmention;
-    use crate::wm_url::Url;
-    use async_std::task::block_on;
-    #[ignore]
-    #[test]
-    fn send_webmention_test() {
-        let source = Url::parse("https://marinintim.com/projects/webmention/").unwrap();
-        let target = Url::parse("https://webmention.rocks/test/4").unwrap();
-        let mut mention = Webmention::from((source, target));
-
-        let result = block_on(mention.send());
-        assert!(result.is_ok());
-        let result = result.unwrap();
-        assert_eq!(result, true);
-    }
-}
